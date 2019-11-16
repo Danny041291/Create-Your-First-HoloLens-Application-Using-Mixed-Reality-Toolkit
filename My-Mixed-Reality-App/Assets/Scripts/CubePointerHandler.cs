20450383fe1743c83fe2f0743db7346259fabc38
danny <dannny@joinpad.net>
2019-11-16 16:01:41 +0100

    Adding CubePointerHandler

---
+++ My-Mixed-Reality-App/Assets/Scripts/CubePointerHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+public class CubePointerHandler : MonoBehaviour, IMixedRealityPointerHandler
+{
+
+    public Material OnPointerDownMaterial;
+    public Material OnPointerUpMaterial;
+    public Material OnPointerClickedMaterial;
+    public Material OnPointerDraggedMaterial;
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        gameObject.GetComponent<MeshRenderer>().material = OnPointerDownMaterial;
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        gameObject.GetComponent<MeshRenderer>().material = OnPointerUpMaterial;
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        /* Decomment the code below to test OnPointerClicked event */
+        // gameObject.GetComponent<MeshRenderer>().material = OnPointerClickedMaterial;
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        /* Decomment the code below to test OnPointerDragged event */
+        // gameObject.GetComponent<MeshRenderer>().material = OnPointerDraggedMaterial;
+    }
+
+
+}
